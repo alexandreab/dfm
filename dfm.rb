96e217b2892b8820cf9ff968c6781afc66907929
Alexandre Barbosa <alexandreab@live.com>
2015-06-10 18:36:17 -0300

    Compare file with others in same directory
    Signed-off-by: Alexandre Barbosa <alexandreab@live.com>

---
+++ dfm.rb
@@ -0,0 +1,33 @@
+require "fileutils"
+args = Hash[ ARGV.join(' ').scan(/--?([^\s]+)(?:\s(\S+))?/) ]
+
+if args.key?('help')
+  help_file = File.open('doc/dfm_help.txt', "r")
+  data = help_file.read
+  puts data
+  Kernel.exit
+end
+
+if not args.key?('file')
+  abort("dfm: missig argument file.\n"+
+        "It is necessary for now.\n"+
+        "Try 'dfm --help' for more information.")
+else
+  target_file_path = args['file']
+end
+
+if not args.key?('directory')
+  target_directory = '.'
+else
+  if not Dir.exist?(args['directory'])
+    abort("dfm: invalid argument directory.\n"+
+          "This directory doesn't exist.\n"+
+          "Try 'dfm --help' for more information.")
+  end
+  target_directory = args['directory']
+end
+
+equal_files = Array.new
+Dir.foreach(target_directory) { |x| equal_files.push(x) if FileUtils.cmp(target_file_path, x) and target_file_path != x }
+
+puts equal_files
